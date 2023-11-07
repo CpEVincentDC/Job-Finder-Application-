56d41c383dac5933e44351e56bd5abbd4cd863db
CpEVincentDC <CpEVincentCumayas@gmail.com>
2023-11-07 09:58:36 +0800

    created search bar containing Search job, company, and location. By adding sorting of contains, type of remote, and level of expertise

--- Job-finder/src/App.jsx
+++ Job-finder/src/App.jsx
@@ -7,7 +7,7 @@ import Value from "./components/ValueDiv/Value";
 
 const App = () => {
   return (
-    <div className="m-auto bg-white">
+    <div className="w-[85%] m-auto bg-white">
       <NavBar />
       <Search />
       <Jobs />
